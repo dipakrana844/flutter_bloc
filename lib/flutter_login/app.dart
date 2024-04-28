fb8e6d3441404799a88f812580eac6c76ed0d621
DRana <dipakrana2001@gmail.com>
2024-04-28 11:02:57 +0530

    -> flutter_login using bloc app.

---
+++ lib/flutter_login/app.dart
@@ -0,0 +1,89 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_test/flutter_login/authentication/authentication.dart';
+import 'package:flutter_bloc_test/flutter_login/home/home.dart';
+import 'package:flutter_bloc_test/flutter_login/login/login.dart';
+import 'package:flutter_bloc_test/flutter_login/splash/splash.dart';
+import 'package:user_repository/user_repository.dart';
+
+class FlutterLogin extends StatefulWidget {
+  const FlutterLogin({super.key});
+
+  @override
+  State<FlutterLogin> createState() => _FlutterLoginState();
+}
+
+class _FlutterLoginState extends State<FlutterLogin> {
+  late final AuthenticationRepository _authenticationRepository;
+  late final UserRepository _userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationRepository = AuthenticationRepository();
+    _userRepository = UserRepository();
+  }
+
+  @override
+  void dispose() {
+    _authenticationRepository.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: _authenticationRepository,
+          userRepository: _userRepository,
+        ),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  HomePage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unknown:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,20 @@
 import 'package:bloc/bloc.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_test/counter_observer.dart';
-import 'package:flutter_bloc_test/image_search/app.dart';
-import 'package:flutter_bloc_test/image_search/data/datasources/remote/image_datasource.dart';
-import 'package:flutter_bloc_test/image_search/data/datasources/remote/image_datasource_impl.dart';
-import 'package:flutter_bloc_test/image_search/data/repositories/image_repository_impl.dart';
-import 'package:flutter_bloc_test/image_search/domain/repositories/image_repository.dart';
-import 'package:flutter_bloc_test/image_search/domain/use_cases/image_use_case.dart';
-import 'package:flutter_bloc_test/image_search/presentation/bloc/image_bloc.dart';
+import 'package:flutter_bloc_test/flutter_login/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const CounterBlocObserver();
-  final ImageDataSource dataSource = ImageDataSourceImpl(dio: Dio());
-  final ImageRepository repository =
-      ImageRepositoryImpl(dataSource: dataSource);
-  final FetchImagesUseCase useCase = FetchImagesUseCase(repository);
-  final ImageBloc bloc = ImageBloc(imagesUseCase: useCase);
+
+  // final ImageDataSource dataSource = ImageDataSourceImpl(dio: Dio());
+  // final ImageRepository repository =
+  //     ImageRepositoryImpl(dataSource: dataSource);
+  // final FetchImagesUseCase useCase = FetchImagesUseCase(repository);
+  // final ImageBloc bloc = ImageBloc(imagesUseCase: useCase);
   // runApp( const CounterApp());
   // runApp( const TimerApp());
   // runApp(const InfiniteApp());
-  runApp(
-    ImageSearchApp(bloc: bloc),
-  );
+  // runApp(ImageSearchApp(bloc: bloc));
+  runApp(const FlutterLogin());
 }
